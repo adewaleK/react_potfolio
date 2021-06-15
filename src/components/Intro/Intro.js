c04a68885ef49ac7ed86418035e0e3740f260e36
kamil <devkamilnaija@gmail.com>
2021-06-15 02:56:12 +0100

    CREATES THE INTRODUCTION PART

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import './Intro.css'
+
 
 function Intro() {
     return (
-        <div>
-            Intro
+        <div className="introduction">
+            <div className="my-pic">
+                <img src="https://lh3.googleusercontent.com/proxy/RsyHXd9LliJzO8Mz7mRN6ytUSAQaSwLEGasG1VQlLwPj674l4CILUGUfo_MtxULxEvx8HuPG21flLL6lLLyAZz2boD4Ek8StD_RnAu0lyzZFa0x_4A" />  
+            </div>
+            <div className="intro-text">
+                <h3>Hello there, I'm</h3>
+                <h1>Kamilu Adewale</h1>
+                <p><span>Fullstack </span>Developer</p>
+            </div>
         </div>
     )
 }
